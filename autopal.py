6958362d22d8343655343eb9fe36b3bd7fef8cc3
Alberto Del Real <alberto.delreal.iv@gmail.com>
2021-10-29 02:19:27 -0400

    Initial commit

---
+++ autopal.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<p>Index Page</p>"
+
+@app.route("/home")
+def home():                     #Note to self, be careful not to duplicate function names
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+#dont worry about this for now
+if __name__ == '__main__':
+    app.run(debug=True)
