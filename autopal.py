f8389fab5fe3efa30ac50df4b46de26cfbaf860a
tswest <89323659+tswest@users.noreply.github.com>
2022-03-19 15:54:52 -0700

    database update

--- autopal.py
+++ autopal.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy  #for database
+
 app = Flask(__name__, static_url_path='/static')
+
+#next section is for database
+app.config['SQLAlchemy_DATABASE_URI'] = 'sql:///site.db'  
+db = SQLAlchemy(app) 
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+#end of section for database
 
 @app.route('/') 
 def index():
